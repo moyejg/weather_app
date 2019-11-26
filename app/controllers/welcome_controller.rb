59e8e01691d17e55e21d57dfb8aba326d8e895bc
Greg Moye <moye.jg@gmail.com>
2019-11-26 10:46:27 -0500

    implemented weather by zip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,30 @@
 class WelcomeController < ApplicationController
   def test
+  	response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=28607&units=imperial&appid=#{ENV['openweather_api_key']}")
+
+  @location = response['name']
+  @temp = response['main']['temp']
+  @weather_icon = response['weather'][0]['icon']
+  @weather_words = response['weather'][0]['description']
+  @cloudiness = response['clouds']['all']
+  @windiness = response['wind']['speed']
+
   end
 
   def index
+  	if params[:zipcode] != '' && !params[:zipcode].nil?
+  		response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+  		@status = response['cod']
+  		if @status != '404' && response['message'] != 'city not found'
+  			@location = response['name']
+  			@temp = response['main']['temp']
+  			@weather_icon = response['weather'][0]['icon']
+  			@weather_words = response['weather'][0]['description']
+  			@cloudiness = response['clouds']['all']
+  			@windiness = response['wind']['speed']
+  		else
+  			@error = response['message']
+  		end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'welcome/test'
+  get 'index' => 'welcome#index'
+  post 'index' => 'welcome#index'
+  get 'test' => 'welcome#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
